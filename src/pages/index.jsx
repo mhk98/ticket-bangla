73a1e632f9d02d347019b95c2b76f0d7a61722fb
mhk98 <mohsinkabirseo@gmail.com>
2023-06-24 22:51:58 +0600

    ticket bangla init

---
+++ src/pages/index.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+//= Packages
+import Head from "next/head";
+//= Layout
+import Layout from "@/layouts/preview";
+//= Components
+import Navbar from "@/components/Preview/Navbar";
+import Overlay from "@/components/Preview/Overlay";
+import Header from "@/components/Preview/Header";
+import Demos from "@/components/Preview/Demos";
+import Marq from "@/components/Preview/Marq";
+import Pages from "@/components/Preview/Pages";
+import ShopDemos from "@/components/Preview/ShopDemos";
+import CallToAction from "@/components/Preview/CallToAction";
+import Footer from "@/components/Preview/Footer";
+import ParallaxSlider from "@/components/Showcases/ParallaxSlider";
+import List from "@/components/Shop/List";
+
+function LandingPreview() {
+  useEffect(() => {
+    document.body.classList.add("sub-bg");
+    return () => document.body.classList.remove("sub-bg");
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Geekfolio - Preview</title>
+      </Head>
+
+      <Navbar />
+      <Overlay />
+      <main>
+        <ParallaxSlider></ParallaxSlider>
+        <List></List>
+        {/* <Header />
+        <Demos />
+        <Marq />
+        <Pages />
+        <ShopDemos />
+        <CallToAction /> */}
+        <Footer />
+      </main>
+    </>
+  );
+}
+
+LandingPreview.getLayout = (page) => <Layout>{page}</Layout>;
+
+export default LandingPreview;
